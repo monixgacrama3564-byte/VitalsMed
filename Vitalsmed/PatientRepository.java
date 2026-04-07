ef212988e2d2d72143b072da7acad0ca2b0f3920
Monix Gacrama <monixgacrama3564@gmail.com>
2026-04-07 17:39:11 +0000

    Fix package name inconsistency in Patient.java
    Corrected package declaration from 'com.trithread.Vitalsmed.model' to 'com.trithread.vitalsmed.model' to match imports in PatientController and PatientRepository. This resolves compilation errors and ensures consistent package naming across the project.

--- Vitalsmed/PatientRepository.java
+++
@@ -1,6 +0,0 @@
-package com.trithread.vitalsmed.repository;
-
-import com.trithread.vitalsmed.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {}
--- Vitalsmed/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.trithread.vitalsmed.repository;
-
-import com.trithread.vitalsmed.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
